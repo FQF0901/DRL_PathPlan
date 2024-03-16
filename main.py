1e25c4f5824fe30b26c8ea5817ec7e14f459fc27
FQF <fangqifei@outlook.com>
2024-03-16 22:42:02 +0800

    entire seems ok but need check

--- main.py
+++ main.py
@@ -12,10 +12,9 @@ start_node = HAS.Node(env.TP[0], env.TP[1], env.TP[2], 0, 0)
 goal_node = HAS.Node(env.SP[0], env.SP[1], env.SP[2], 0, 0)
 obstacles = env.obj + env.other_veh
 
-path = HAS.hybrid_a_star(start_node, goal_node, obstacles)
-
-if path:
+path, RSpath = HAS.hybrid_a_star(start_node, goal_node, obstacles)
+if path and RSpath:
     print("找到路径！")
-    HAS.Path_show(path, obstacles, start_node, goal_node)
+    HAS.Path_show(path, RSpath, obstacles, start_node, goal_node)
 else:
     print("未找到路径！")
