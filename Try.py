45366014213f1251f22c1849de66098213b7dba0
FQF <fangqifei@outlook.com>
2024-03-16 21:33:00 +0800

    RS ok

--- Try.py
+++ Try.py
@@ -8,22 +8,22 @@ import ParaCfg
 import numpy as np
 import Env
 
-env = Env.Env()
-env.reset()
+# env = Env.Env()
+# env.reset()
 
 # 计算路径
-path = rs.calc_optimal_path(env.SP[0], env.SP[1], env.SP[2], env.TP[0], env.TP[1], env.TP[2], ParaCfg.VehPara.radius, 0.2)
+path = rs.calc_optimal_path(0, 0, 0, 5, 5, math.radians(-120), 0.2, 0.02)
 
 
-# 提取路径中的点坐标
+# # 提取路径中的点坐标
 x_coords = path.x
 y_coords = path.y
 
 # 绘制路径
 plt.figure()
 plt.plot(x_coords, y_coords, 'b-')
-plt.plot(env.SP[0], env.SP[1], 'ro')  # 起点
-plt.plot(env.TP[0], env.TP[1], 'go')  # 终点
+plt.plot(0, 0, 'ro')  # 起点
+plt.plot(5, 5, 'go')  # 终点
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.title('Path Visualization')
